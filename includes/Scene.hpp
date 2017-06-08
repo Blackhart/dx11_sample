d1d520fac7b5b04a6bec0c8d2cee4854945c5c50
Thomas Manceau <manceau.thomas19@gmail.com>
2017-06-08 16:08:01 +0200

    Architecture refactoring: 	- Simple scene class added. Will handle all the Mesh into the 	  scene

--- includes/Scene.hpp
+++ includes/Scene.hpp
@@ -2,6 +2,8 @@
 #define __SCENE_HPP__
 
 #include <vector>
+#include <cstdint>
+#include <stdexcept>
 
 #include "Mesh.hpp"
 
@@ -12,8 +14,11 @@ public:
 	Scene(Scene const&) = delete;
 	Scene(Scene&&) = delete;
 	~Scene() = default;
-	Scene&	operator=(Scene const&) = delete;
-	Scene&	operator=(Scene&&) = delete;
+	Scene&		operator=(Scene const&) = delete;
+	Scene&		operator=(Scene&&) = delete;
+	void		AddObject(Mesh* const pMesh);
+	Mesh* const	GetObjectAt(uint32_t pIndex) const;
+	uint32_t	GetObjectCount() const;
 private:
 	std::vector<Mesh*>	__objects;
 };
--- sources/Scene.cpp
+++ sources/Scene.cpp
@@ -1,1 +1,18 @@
 #include "../includes/Scene.hpp"
+
+void	Scene::AddObject(Mesh* const pMesh)
+{
+	__objects.push_back(pMesh);
+}
+
+Mesh* const	Scene::GetObjectAt(uint32_t pIndex) const
+{
+	if (pIndex < GetObjectCount())
+		throw new std::out_of_range("Scene::GetObjectAt() : Invalid Range Exception!");
+	return __objects[pIndex];
+}
+
+uint32_t	Scene::GetObjectCount() const
+{
+	return __objects.size();
+}
